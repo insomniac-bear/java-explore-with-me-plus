03384bf4326472b1fdd9b880d560eddd2dec9f9a
AndreyAA-code <otd53@yandex.ru>
2025-12-18 23:43:20 +0300

    added EnpointHit model dto, Stats controller, service, repo, tests

---
+++ stats/stats-server/src/test/java/ru/practicum/EWM_Tests.java
@@ -0,0 +1,14 @@
+package ru.practicum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EWM_Tests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
