4279398c89fd9d87c6479200ebd760e7bb5a92b0
AndreyAA-code <otd53@yandex.ru>
2025-12-24 16:55:34 +0300

    users: structure, model, dtos, exception, controller, mapper

--- main-service/src/main/java/ru/practicum/service/UserServiceImpl.java
+++ main-service/src/main/java/ru/practicum/service/UserServiceImpl.java
@@ -1,16 +1,25 @@
 package ru.practicum.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.practicum.dto.NewUserRequestDto;
 import ru.practicum.dto.UserDto;
+import ru.practicum.mapper.UserMapper;
+import ru.practicum.repository.UserRepositoiry;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @Service
 public class UserServiceImpl implements UserService {
+    private final UserRepositoiry userRepositoiry;
+    private final UserMapper userMapper;
+
+
     @Override
     public List<UserDto> getUsers(Long userId) {
-        return List.of();
+
+        return userRepositoiry.getAllUsers();
     }
 
     @Override
