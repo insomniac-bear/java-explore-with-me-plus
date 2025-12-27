5c15f911bf6f7e8fb274f0f78da67cd24f2bb5c8
Alexander <alexanderskipper@yandex.ru>
2025-12-27 13:08:28 +0400

    Сохранить изменения schema.sql перед применением stash

--- main-service/src/main/resources/schema.sql
+++ main-service/src/main/resources/schema.sql
@@ -10,3 +10,19 @@ CREATE TABLE IF NOT EXISTS categories (
     id BIGSERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL UNIQUE
 );
+
+CREATE TABLE IF NOT EXISTS events (
+    id BIGSERIAL PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS compilations (
+    id     BIGSERIAL PRIMARY KEY,
+    title  VARCHAR(255) NOT NULL,
+    pinned BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE IF NOT EXISTS compilation_events (
+    compilation_id BIGINT NOT NULL REFERENCES compilations(id) ON DELETE CASCADE,
+    event_id       BIGINT NOT NULL REFERENCES events(id),
+    CONSTRAINT pk_compilation_events PRIMARY KEY (compilation_id, event_id)
+);
