1ab06b87e727482234815b15dcf6c078ba98b88d
AndreyAA-code <otd53@yandex.ru>
2026-01-03 12:36:39 +0300

    Admin event: added controller

--- main-service/src/main/java/ru/practicum/repository/CompilationRepository.java
+++ main-service/src/main/java/ru/practicum/repository/CompilationRepository.java
@@ -1,12 +1,15 @@
 package ru.practicum.repository;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import ru.practicum.dto.CompilationDto;
 import ru.practicum.model.Compilation;
 
 import java.util.List;
 
 public interface CompilationRepository extends JpaRepository<Compilation, Long> {
-    List<Compilation> findAll(Boolean pinned, Pageable pageable);
+
+    Page<Compilation> findAll(Boolean pinned, Pageable pageable);
+
+    Page<Compilation> findAll(Pageable pageable);
 }
