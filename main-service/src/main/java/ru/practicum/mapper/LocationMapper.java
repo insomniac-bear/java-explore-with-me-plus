0782208e3cc22210ab141ab6adb8a1141295601d
AndreyAA-code <otd53@yandex.ru>
2026-01-12 14:10:45 +0300

    added location(s) table, entity, enum for locationType, service, repository

--- main-service/src/main/java/ru/practicum/mapper/LocationMapper.java
+++ main-service/src/main/java/ru/practicum/mapper/LocationMapper.java
@@ -1,20 +1,22 @@
 package ru.practicum.mapper;
 
-import org.mapstruct.Mapper;
+import org.mapstruct.*;
 import ru.practicum.dto.location.LocationResponseDto;
 import ru.practicum.dto.location.NewLocationDto;
 import ru.practicum.dto.location.ShortLocationResponseDto;
 import ru.practicum.dto.location.UpdateLocationDto;
 import ru.practicum.model.Location;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface LocationMapper {
 
     LocationResponseDto toFullResponseDto(Location location);
 
     ShortLocationResponseDto toShortResponseDto(Location location);
 
+    @Mapping(target = "id", ignore = true)
     Location toLocation(NewLocationDto dto);
 
-    void updateFromDto(UpdateLocationDto dto, Location existingLocation);
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE, ignoreByDefault = false)
+    void updateFromDto(UpdateLocationDto dto, @MappingTarget Location entity);
 }
