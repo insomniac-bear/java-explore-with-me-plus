bf9e63e774aa594fea43a0131a4aa38211279afd
AndreyAA-code <otd53@yandex.ru>
2026-01-18 15:24:14 +0300

    checked errors

--- main-service/src/main/java/ru/practicum/model/Location.java
+++ main-service/src/main/java/ru/practicum/model/Location.java
@@ -3,7 +3,6 @@ package ru.practicum.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 import lombok.*;
-import org.hibernate.validator.constraints.Length;
 import ru.practicum.util.LocationType;
 
 @Entity
